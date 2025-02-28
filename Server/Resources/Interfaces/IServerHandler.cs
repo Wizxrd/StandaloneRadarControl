4ffe8ecd94accabfdbc458f8d2dca64a9109ca33
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-02-27 23:37:00 -0500

    Server Handler interface so different methods can be swapped in and out.

---
+++ Server/Resources/Interfaces/IServerHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Server;
+
+public interface IServerHandler
+{
+	public int ClientCount { get; set; }
+	float PingInterval { get; set; }
+	int UpdatesPerSecond { get; set; }
+	int ActivePort { get; set; }
+	
+	bool StartServer();
+	bool StopServer();
+}
